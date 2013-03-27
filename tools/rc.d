b9d5f0bc414f8b582e14faa85cd4d3915aa5d4ce
Mattias Andrée <maandree@operamail.com>
2013-03-27 06:50:32 +0100

    code style
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- tools/rc.d
+++ tools/rc.d
@@ -5,8 +5,8 @@
 
 # print usage and exit
 usage() {
-	local name=${0##*/}
-	cat >&2 << EOF
+    local name=${0##*/}
+    cat >&2 << EOF
 usage: $name <action> [options] [daemons]
 
 options:
@@ -25,27 +25,27 @@ e.g: $name list
      $name start sshd gpm
      $name help
 EOF
-	exit ${1:-1}
+    exit ${1:-1}
 }
 
 # filter list of daemons
 filter_daemons() {
-	local -a new_daemons=()
-	for daemon in "${daemons[@]}"; do
-		# check if daemons is valid
-		if ! have_daemon "$daemon"; then
-			printf "${C_FAIL}:: ${C_DONE}Daemon script ${C_FAIL}${daemon}${C_DONE} does \
+    local -a new_daemons=()
+    for daemon in "${daemons[@]}"; do
+        # check if daemons is valid
+        if ! have_daemon "$daemon"; then
+            printf "${C_FAIL}:: ${C_DONE}Daemon script ${C_FAIL}${daemon}${C_DONE} does \
 not exist or is not executable.${C_CLEAR}\n" >&2
-			exit 2
-		fi
-		# check filter
-		(( ${filter[started]} )) && ck_daemon "$daemon" && continue
-		(( ${filter[stopped]} )) && ! ck_daemon "$daemon" && continue
-		(( ${filter[auto]} )) && ck_autostart "$daemon" && continue
-		(( ${filter[noauto]} )) && ! ck_autostart "$daemon" && continue
-		new_daemons+=("$daemon")
-	done
-	daemons=("${new_daemons[@]}")
+            exit 2
+        fi
+        # check filter
+        (( ${filter[started]} )) && ck_daemon "$daemon" && continue
+        (( ${filter[stopped]} )) && ! ck_daemon "$daemon" && continue
+        (( ${filter[auto]} )) && ck_autostart "$daemon" && continue
+        (( ${filter[noauto]} )) && ! ck_autostart "$daemon" && continue
+        new_daemons+=("$daemon")
+    done
+    daemons=("${new_daemons[@]}")
 }
 
 (( $# < 1 )) && usage
@@ -63,13 +63,13 @@ eval set -- "$argv"
 
 # create an initial daemon list
 while [[ "$1" != -- ]]; do
-	case "$1" in
-		-s|--started)		filter[started]=1 ;;
-		-S|--stopped)		filter[stopped]=1 ;;
-		-a|--auto)			filter[auto]=1 ;;
-		-A|--noauto)		filter[noauto]=1 ;;
-	esac
-	shift
+    case "$1" in
+        -s|--started)        filter[started]=1 ;;
+        -S|--stopped)        filter[stopped]=1 ;;
+        -a|--auto)            filter[auto]=1 ;;
+        -A|--noauto)        filter[noauto]=1 ;;
+    esac
+    shift
 done
 
 # remove --
@@ -80,55 +80,54 @@ shift
 
 # get initial daemons list
 for daemon; do
-	daemons+=("$daemon")
+    daemons+=("$daemon")
 done
 
 # going into script directory
 cd /etc/rc.d
 
 case $action in
-	help)
-		usage 0 2>&1
-	;;
-	list)
-		# list take all daemons by default
-		[[ -z $daemons ]] && for d in *; do have_daemon "$d" && daemons+=("$d"); done
-		filter_daemons
-		for daemon in "${daemons[@]}"; do
-			# print running / stopped satus
-			if ! ck_daemon "$daemon"; then
-				s_status="${C_OTHER}[${C_DONE}STARTED${C_OTHER}]"
-			else
-				s_status="${C_OTHER}[${C_FAIL}STOPPED${C_OTHER}]"
-			fi
-			# print auto / manual status
-			if ! ck_autostart "$daemon"; then
-				s_auto="${C_OTHER}[${C_DONE}AUTO${C_OTHER}]"
-			else
-				s_auto="${C_OTHER}[${C_FAIL}    ${C_OTHER}]"
-			fi
-			printf "$s_status$s_auto${C_CLEAR} $daemon\n"
-		done
-	;;
-	*)
-		# other actions need an explicit daemons list
-		[[ -z $daemons ]] && usage
-		filter_daemons
-		# set same environment variables as init
-		runlevel=$(/sbin/runlevel)
-		ENV=('PATH=/bin:/usr/bin:/sbin:/usr/sbin'
-			"PREVLEVEL=${runlevel%% *}"
-			"RUNLEVEL=${runlevel##* }"
-			"CONSOLE=${CONSOLE:-/dev/console}"
-			"TERM=$TERM")
-		cd /
-		for daemon in "${daemons[@]}"; do
-			env -i "${ENV[@]}" "/etc/rc.d/$daemon" "$action"
-			(( ret += !! $? ))  # clamp exit value to 0/1
-		done
-	;;
+    help)
+        usage 0 2>&1
+    ;;
+    list)
+        # list take all daemons by default
+        [[ -z $daemons ]] && for d in *; do have_daemon "$d" && daemons+=("$d"); done
+        filter_daemons
+        for daemon in "${daemons[@]}"; do
+            # print running / stopped satus
+            if ! ck_daemon "$daemon"; then
+                s_status="${C_OTHER}[${C_DONE}STARTED${C_OTHER}]"
+            else
+                s_status="${C_OTHER}[${C_FAIL}STOPPED${C_OTHER}]"
+            fi
+            # print auto / manual status
+            if ! ck_autostart "$daemon"; then
+                s_auto="${C_OTHER}[${C_DONE}AUTO${C_OTHER}]"
+            else
+                s_auto="${C_OTHER}[${C_FAIL}    ${C_OTHER}]"
+            fi
+            printf "$s_status$s_auto${C_CLEAR} $daemon\n"
+        done
+    ;;
+    *)
+        # other actions need an explicit daemons list
+        [[ -z $daemons ]] && usage
+        filter_daemons
+        # set same environment variables as init
+        runlevel=$(/sbin/runlevel)
+        ENV=('PATH=/bin:/usr/bin:/sbin:/usr/sbin'
+            "PREVLEVEL=${runlevel%% *}"
+            "RUNLEVEL=${runlevel##* }"
+            "CONSOLE=${CONSOLE:-/dev/console}"
+            "TERM=$TERM")
+        cd /
+        for daemon in "${daemons[@]}"; do
+            env -i "${ENV[@]}" "/etc/rc.d/$daemon" "$action"
+            (( ret += !! $? ))  # clamp exit value to 0/1
+        done
+    ;;
 esac
 
 exit $ret
 
-# vim: set ts=2 sw=2 ft=sh noet:
