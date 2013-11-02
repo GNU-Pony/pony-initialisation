6f0d6135992b7b8d0c9ebab4b00c6604bc684579
Mattias Andrée <maandree@operamail.com>
2013-11-03 00:40:00 +0100

    add /etc/profile.d/use_colour.sh
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

---
+++ conf/profile.d/use_colour.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+unset USECOLOUR
+
+USECOLOUR="$(. "£{ETC}/rc.conf" 2>"£{DEV}/null"; echo "${USECOLOUR}")"
+USECOLOUR="${USECOLOUR,,}"
+
+if [ "${USECOLOUR}" = "auto" ]: then
+    usedtty="$(tty)"
+    USECOLOUR="no"
+    if [ "${usedtty}" = "£{DEV}/console" ]; then
+	USECOLOUR="yes"
+    else
+	usedtty="${usedtty//[0-9]/}"
+	if [ "${usedtty}" = "£{DEV}/tty" ] || [ "${usedtty}" = "£{DEV}/pts/" ]; then
+	    USECOLOUR="yes"
+	fi
+    fi
+fi
+
+if [ "${USECOLOUR}" = y ] || [ "${USECOLOUR}" = 1 ] || [ "${USECOLOUR}" = yes ]; then
+    USECOLOUR=yes
+else
+    USECOLOUR=no
+fi
+
+
+export USECOLOUR
+export USECOLOURS=${USECOLOUR}
+export USECOLOR
+export USECOLORS=${USECOLOUR}
+export USE_COLOUR
+export USE_COLOURS=${USECOLOUR}
+export USE_COLOR
+export USE_COLORS=${USECOLOUR}
+
